2bbbbb412adf4d6d67d31200c6742da363d21c5e
gabin.laborieux <gabinlaborieux@gmail.com>
2025-10-08 15:46:11 +0200

    v2 tp dev efficace

--- Developpement_efficace/TPs/src/genericite/Noeud.java
+++ Developpement_efficace/TPs/src/genericite/Noeud.java
@@ -8,7 +8,7 @@ package genericite;
  *
  * @param <T>
  */
-public class Noeud<T> {
+public class Noeud<T extends Comparable<T>> {
 
     private final T valeur;
     private Noeud<T> gauche; // attribut du même type que la classe
@@ -24,4 +24,38 @@ public class Noeud<T> {
         this.gauche = null;
         this.droit = null;
     }
+
+    /**
+     * Indique si la valeur cherchée est présente dans l'arbre débutant au nœud courant.
+     * @param valeurCherchee valeur recherchée
+     * @return true si présente, false sinon
+     */
+    public boolean estPresente(T valeurCherchee) {
+        int cmp = valeurCherchee.compareTo(this.valeur);
+        // être plus strict,en appliquant le type T. Donc ici type doit implémenté la classe comparable
+
+        if (cmp == 0) {
+            return true;
+        } else if (cmp < 0) {
+            return (gauche != null) && gauche.estPresente(valeurCherchee);
+        } else {
+            return (droit !=null) && droit.estPresente(valeurCherchee);
+        }
+    }
+
+    /**
+     *
+     * @param aInserer valeur à insérer
+     * @return true si l'insertion a été effectuée, false si la valeur existait déjà
+     */
+    public boolean inserer(T aInserer) {
+        int cmp = aInserer.compareTo(this.valeur);
+        if (cmp == 0) {
+            return false; // Pas de doublons
+        } else if (cmp < 0) {
+            if (gauche == null) {
+                gauche = new Noeud<>(aInserer);
+                return true;
+            }
+
 }
